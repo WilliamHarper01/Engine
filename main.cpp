37e9120ea8b90e09e3df504c0fdb0df6b52765f1
William Harper <williamharper@trentu.ca>
2022-06-09 22:05:43 -0400

    added Vulkan Memory Allocator

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
-#include "test.h"
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
 int main()
 {
+	GLFWwindow* window;
+	glfwInit();
+	window = glfwCreateWindow(1920, 1080, "bruh", NULL, NULL);
+	VkInstance i;
+	vkCreateInstance(nullptr, nullptr, &i);
+	
 	std::cout << "test\n";
-	return test();
+	return 0;
 }
